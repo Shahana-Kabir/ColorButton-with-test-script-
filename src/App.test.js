d98711ada24806a989a417e1034a4fab0454cc98
Shahana Kabir <shahana.kabir@gmail.com>
2022-05-04 13:04:00 -0700

    button changes color and checkbox feature added with it

--- src/App.test.js
+++ src/App.test.js
@@ -8,8 +8,32 @@ test('button has correct initial color', () => {
   fireEvent.click(colorButton);
   expect(colorButton).toHaveStyle({backgroundColor:'blue'});
   expect(colorButton.textContent).toBe('Change to red');
-
   
 });
+
+
+test('initial conditions', ()=> {
+  render(<App />)
+  // check that the button starts out enabled
+ 
+const colorButton = screen.getByRole("button", {name: 'Change to blue'});
+expect(colorButton).toBeEnabled();
+  // check that the chekbox starts out unchecked
+
+  const checkedBox = screen.getByRole("checkbox");
+  expect(checkedBox).not.toBeChecked();
+})
+
+test('Checkbox disables button on first click and enables on second click',()=> {
+  render(<App />)
+  const checkbox = screen.getByRole('checkbox');
+  const button = screen.getByRole('button');
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+})
 
 
